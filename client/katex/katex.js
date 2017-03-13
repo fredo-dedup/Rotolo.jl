f1e88dce10119b89d927eb558d7bc82fc5c37af9
fredo <frederic.testard@gmail.com>
2017-03-14 00:21:35 +0100

    wip

--- client/katex/katex.js
+++ client/katex/katex.js
@@ -1,58 +1,5 @@
 import katex from './katex.min.js'
-
-// require("vue-style-loader!raw-loader!./kateex.min.css")
-// require("./katex.min.css")
 require("https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css")
-
-//
-// var fileref = document.createElement("link")
-// fileref.setAttribute("rel", "stylesheet")
-// fileref.setAttribute("type", "text/css")
-// fileref.setAttribute("href", "./katex.min.css")
-// //
-// import katexcss from './katex.min.css'
-
-// add the katex style file to head
-// var css = 'h1 { background: red; }'
-// var reader = new FileReader();
-// var cssFile = new File(['aaaa'], 'D:/frtestar/.julia/v0.5/Rotolo/client/kateex.min.css', {type: 'text/plain'});
-//
-// reader.onload = function(evt) {
-//   var head = document.head || document.getElementsByTagName('head')[0]
-//   var style = document.createElement('style');
-//
-//   console.log('css file read')
-//   console.log(evt.target)
-//
-//   style.type = 'text/css';
-//   if (style.styleSheet){
-//     style.styleSheet.cssText = evt.target.result;
-//   } else {
-//     style.appendChild(document.createTextNode(evt.target.result));
-//   }
-//   head.appendChild(style);
-// }
-//
-// reader.onerror = function(evt) {
-//   console.log('error reading css file')
-// }
-//
-// reader.readAsText(cssFile)
-
-
-// var css = reader.readAsText(File("./katex.min.css"))
-//
-// var head = document.head || document.getElementsByTagName('head')[0],
-//     style = document.createElement('style');
-//
-// style.type = 'text/css';
-// if (style.styleSheet){
-//   style.styleSheet.cssText = css;
-// } else {
-//   style.appendChild(document.createTextNode(css));
-// }
-// head.appendChild(style);
-
 
 export default {
 
--- src/test-3.jl
+++ src/test-3.jl
@@ -1,80 +1,166 @@
 
-sessions = Dict{String, Channel{String}}()
+
+immutable Session
+  channel::Channel{String}
+  port::Int
+end
+
+sessions = Dict{String, Session}()
 
 import Base.send
 import JSON
 
-function send(sid::String, nid::Int, command::String; args...)
+function send(sid::String, nid::Int, command::String,
+              params::Dict{Symbol,Any})
   msg = Dict{Symbol, Any}()
   msg[:nid] = nid
   msg[:command] = command
-  for (k,v) in args
-    msg[k] = v
-  end
-  println(JSON.json(msg))
+  msg[:params] = params
+
   if haskey(sessions, sid)
-    put!(sessions[sid], JSON.json(msg))
+    put!(sessions[sid].channel, JSON.json(msg))
   else
     error("[send] no session $sid registered")
   end
 end
 
-msgc = Channel{String}(5)
-
-sessions["abcd"] = msgc
-
-send("abcd", 1, "append", newnid=3, params=Dict(:html => "coucou"))
-
-
-@async (m = take!(msgc) ; println(m))
-
-@async (for m in msgc ; println(m) ; end ; println("out of loop"))
-put!(msgc, "helloooo")
-close(msgc)
-
-#find open port
-port, sock = listenany(5000) # find an available port
-close(sock)
-serverid = @async serve(static, comm, port)
-Int(port)
-
-put!(msgc, JSON.json(msg))
 macro session(args...)
   sessionId = length(args)==0 ? randstring() : string(args[1])
 
   if sessionId in keys(sessions) # already opened, just clear the page
     send(sessionId, 0, "clear")
   else # create page
-
-
-
+    xport, sock = listenany(5000) # find an available port
+    close(sock)
+    port = Int(xport)
+    chan = Channel{String}(10)
+    sessions[sessionId] = Session(chan, port)
+    launchServer(chan, port)
+    spinPage(sessionId, port)
+  end
 end
 
-string(45.6)
+using HttpServer
+using WebSockets
 
-sid = tempname()
-tmppath = string(sid, ".rotolo.html")
-scriptpath = joinpath(dirname(@__FILE__), "../client/build.js")
-scriptpath = "D:/frtestar/devl/paper-client/dist/build.js"
+function launchServer(chan::Channel, port::Int)
 
-open(tmppath, "w") do io
-  title = basename(sid)
-  println(io,
-    """
-    <html>
-      <head>
-        <title>$title</title>
-        <meta charset="UTF-8">
-        <script>
-          sessionId = '$sid'
-          serverPort = '$(Int(port))'
-        </script>
-        <script src='$scriptpath'></script>
-      </head>
-      <body></body>
-    </html>
-    """)
+  wsh = WebSocketHandler() do req,client
+    for m in chan
+      println("sending $m")  # msg = read(client)
+      write(client, m)
+    end
+    println("exiting send loop for port $port")
+  end
+
+  handler = HttpHandler() do req, res
+    rsp = Response(100)
+    rsp.headers["Access-Control-Allow-Origin"] =
+      "http://localhost:8080"
+    rsp.headers["Access-Control-Allow-Credentials"] =
+      "true"
+    rsp
+  end
+
+  @async run(Server(handler, wsh), port)
 end
 
-run(`xdg-open $tmppath`)
-run(`cmd /c start $tmppath`)
+function spinPage(sname::String, port::Int)
+  sid = tempname()
+  tmppath = string(sid, ".html")
+  # scriptpath = joinpath(dirname(@__FILE__), "../client/build.js")
+  # scriptpath = "D:/frtestar/devl/paper-client/dist/build.js"
+  scriptpath = "/home/fred/Documents/Dropbox/devls/paper-client/dist/build.js"
+
+  open(tmppath, "w") do io
+    println(io,
+      """
+      <html>
+        <head>
+          <title>$sname</title>
+          <meta charset="UTF-8">
+          <script>
+            serverPort = '$port'
+          </script>
+          <script src='$scriptpath'></script>
+        </head>
+        <body></body>
+      </html>
+      """)
+  end
+
+  run(`xdg-open $tmppath`)
+  # run(`cmd /c start $tmppath`)
+end
+
+##################################################################
+
+
+@session test3
+
+send("test2", 1, "append",
+     params=Dict(:newnid => 100,
+                 :compname => "html-node",
+                 :html=>"hello from Julia"))
+
+using Base.Markdown
+
+str = md"test **test** test"
+typeof(str)
+methodswith(Markdown.MD)
+
+
+strbuf = IOBuffer()
+show(strbuf, MIME"text/html"(), str)
+
+send("test", 1, "append",
+     params=Dict(:newnid => 101,
+                 :compname => "html-node",
+                 :html=>strbuf))
+
+####################################################""
+import Base.Multimedia.writemime
+
+
+function rewire{T<:MIME}(func::Function, mt::Type{T}, t::Type)
+   if method_exists(writemime, (IO, mt, t))
+       meth = methods(writemime, (IO, mt, t))[1].func
+       @eval quote
+               function writemime(io::IO, mt::$mt, x::$t)
+
+
+                   t_task()==currentTask && ($func)(x) # send only if interactive task
+                   ($meth)(io, mt, x)
+               end
+            end
+   else
+       @eval quote
+               function writemime(io::IO, mt::$mt, x::$t)
+                   current_task()==currentTask && ($func)(x)
+               end
+            end
+   end
+   nothing
+end
+
+rewire(func::Function, t::Type) = rewire(func,       MIME"text/plain", t)
+rewire(t::Type)                 = rewire(addtochunk, MIME"text/plain", t)
+
+macro rewire(args...)
+   for a in args
+       try
+           t = eval(a)
+           if isa(t, Type)
+               rewire(t)
+           else
+               warn("$a does not evaluate to a type")
+           end
+       catch e
+           warn("can't evaluate $a, error $e")
+       end
+   end
+   nothing
+end
+
+
+@rewire Base.Markdown.MD Tile Escher.TileList
