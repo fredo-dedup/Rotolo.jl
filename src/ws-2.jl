da237d903e2695925d3350ba7592e7b49c1b3c6c
Frédéric Testard <frederic.testard@gmail.com>
2017-03-13 18:21:51 +0100

    Add files via upload

--- src/ws-2.jl
+++ src/ws-2.jl
@@ -1,3 +1,111 @@
+<<<<<<< HEAD
+using HttpServer
+using WebSockets
+
+msgc = Channel{String}(5)
+
+wsh2 = WebSocketHandler() do req,client
+  try
+    for m in msgc
+
+      println("sending $m")  # msg = read(client)
+      write(client, m)
+    end
+  catch e
+    println("exiting send loop")
+  end
+end
+
+handler = HttpHandler() do req, res
+  rsp = Response(100)
+  rsp.headers["Access-Control-Allow-Origin"] =
+    "http://localhost:8080"
+  rsp.headers["Access-Control-Allow-Credentials"] =
+    "true"
+  rsp
+end
+
+tsk = @async run(Server(handler, wsh2),
+                 8081)
+
+# close(msgc)
+
+##############################################################
+
+id = 200
+newid() = (global id ; id += 1 ; id)
+
+##############################################################
+
+import JSON
+
+msg = Dict(:nid      => 1,
+           :command => "append",
+           :payload => Dict(:nid => newid(),
+                            :params => Dict(:html => "html text"),
+                            :compname => "html-node",
+                            :style  => "color:red"))
+
+put!(msgc, JSON.json(msg))
+
+msg2 = Dict(:nid     => 212,
+            :command => "append",
+            :payload => Dict(:nid => newid(),
+                             :params => Dict(:html => "inner html text"),
+                             :compname => "html-node" ) )
+
+put!(msgc, JSON.json(msg2))
+
+msg3 = Dict(:nid     => 1,
+            :command => "append",
+            :payload => Dict(:nid => newid(),
+                             :params => Dict(:msg => "test message"),
+                             :compname => "testcomp" ) )
+
+put!(msgc, JSON.json(msg3))
+
+
+msg4 = Dict(:nid     => 1,
+            :command => "append",
+            :payload => Dict(:nid => newid(),
+                             :params => Dict(:expr => "c = \\pm\\sqrt{a^2 + b^2}",
+                                             :options => Dict(:displayMode => true ) ),
+                             :compname => "katex" ) )
+
+put!(msgc, JSON.json(msg4))
+
+
+
+id = 200
+newid() = (global id ; id += 1 ; id)
+
+newid()
+
+msg = Dict(:nid      => 1,
+           :command => "append",
+           :payload => Dict(:nid => newid(),
+                            :html => "<button type='button'>Click Me!</button>"))
+put!(msgc, JSON.json(msg))
+
+
+msg3 = Dict(:nid     => 1,
+            :command => "append",
+            :payload => Dict(:nid => 3,
+                             :html => "container",
+                             :style => "display:flex"))
+
+put!(msgc, JSON.json(msg3))
+
+msg3 = Dict(:nid     => 3,
+            :command => "append",
+            :payload => Dict(:nid => 31,
+                             :html => "-element2-"))
+
+put!(msgc, JSON.json(msg3))
+
+
+gensym("comp")
+=======
 using HttpServer
 using WebSockets
 
@@ -94,3 +202,4 @@ put!(msgc, JSON.json(msg3))
 
 
 gensym("comp")
+>>>>>>> 547f1e056144201736439940d9c0ed1c1bb295fc
