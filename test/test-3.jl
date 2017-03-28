5ed2102adb27d7c2da250f690bc47c52a45dae93
fredo <frederic.testard@gmail.com>
2017-03-28 18:23:04 +0200

    wip

--- test/test-3.jl
+++ test/test-3.jl
@@ -190,18 +190,60 @@ end
 
 ############################################################
 
-reload("Wkhtmltox")
+source = IOBuffer("ancd")
+
+mktempdir() do tdir
+  println(tdir)
+  htmlpath = joinpath(tdir, randstring() * ".html")
+  open(io -> write(io, read(source)), htmlpath, "w")
+  println(read(htmlpath))
+end
+
+
+  # source = IOBuffer(read("c:/temp/vegalite.html"))
+
+
+
+
+reload("PhantomJS")
 
 module Try
 end
 
+
 module Try
 
-using Wkhtmltox
+using PhantomJS
+using VegaLite
+
+ts = sort(rand(10))
+ys = Float64[ rand()*0.1 + cos(x) for x in ts]
+
+v = data_values(time=ts, res=ys) +    # add the data vectors & assign to symbols 'time' and 'res'
+      mark_line() +                   # mark type = line
+      encoding_x_quant(:time) +       # bind x dimension to :time, quantitative scale
+      encoding_y_quant(:res) ; nothing
+
+pio = IOBuffer()
+VegaLite.writehtml(pio, v)
+position(pio)
+seekstart(pio)
+
+pngdata = renderhtml(pio)
+
+open(io -> write(io, pngdata), "c:/temp/result.png", "w")
+
+tmppath = "c:/temp/vegalite.html"
+open(io -> VegaLite.writehtml(io, v), tmppath, "w")
+
+
+
 
 
 exepath = joinpath(Pkg.dir("Wkhtmltox"), "deps/src/bin")
 run(`$exepath/wkhtmltopdf c:/temp/example.html c:/temp/example.pdf`)
+
+run(`$exepath/wkhtmltopdf -H`)
 
 pdf_init(1)
 ps = PdfSettings("out" => "c:/temp/example.pdf")
